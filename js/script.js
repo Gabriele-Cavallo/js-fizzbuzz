3f697bf7a0485ad1498f9dccdabd0998c96229c2
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-07 13:36:30 +0100

    feat: add if/else rules

--- js/script.js
+++ js/script.js
@@ -4,10 +4,21 @@
 
 // 1 - Stampo in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
-    console.log('number' , i);
-}
+
 // 2 - Verfico per che numeri è divisibile e assegno il valore corretto
     // 2-a Controllo se il numero è divisibile per 5 E 3 e gli assegno il valore fizzbuzz
     // 2-b Controllo se il numero è divisibile per 5 e gli assegno il valore buzz
     // 2-c Controllo se il numero è divisibile per 3 e gli assegno il valore fizz
+    let number;
+    if (i % 3 === 0 && i % 5=== 0) {
+        number = 'FizzBuzz';
+    }else if (i % 5 === 0) {
+        number = 'Buzz';
+    }else if (i % 3 === 0) {
+        number = 'Fizz';
+    }else{
+        number = i;
+    }
+    console.log(number);
+}
 // 3 - Stampo in console i nuovi valori
