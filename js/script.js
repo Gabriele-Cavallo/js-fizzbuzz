ca094397e6f0623af3290b1b5a48bbc8ff6cb274
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-07 13:17:05 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// TASK -Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// 1 - Stampo in console i numeri da 1 a 100
+// 2 - Verfico per che numeri è divisibile e assegno il valore corretto
+    // 2-a Controllo se il numero è divisibile per 5 E 3 e gli assegno il valore fizzbuzz
+    // 2-b Controllo se il numero è divisibile per 5 e gli assegno il valore buzz
+    // 2-c Controllo se il numero è divisibile per 3 e gli assegno il valore fizz
+// 3 - Stampo in console i nuovi valori
