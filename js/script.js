25a7fd6cc2fce9f7c7154ddb8a6286edf7d5b4ac
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-07 16:16:32 +0100

    feat: add second container

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // BONUS 1:
-// Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
+// // Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
 // 1 - Stampo in console i numeri da 1 a 100
+//Primo container
 for (let i = 1; i <= 100; i++){
-
 // 2 - Verfico per che numeri è divisibile e assegno il valore corretto
     // 2-a Controllo se il numero è divisibile per 5 E 3 e gli assegno il valore fizzbuzz
     // 2-b Controllo se il numero è divisibile per 5 e gli assegno il valore buzz
@@ -29,3 +31,23 @@ for (let i = 1; i <= 100; i++){
     let box = `<div class="box ${number}">${number}</div>`;
     numberBox.innerHTML += box;
 }
+//Secondo container
+for (let i = 1; i <= 100; i++){
+        let number;
+        if (i % 3 === 0 && i % 5=== 0) {
+            number = 'FizzBuzz';
+        }else if (i % 5 === 0) {
+            number = 'Buzz';
+        }else if (i % 3 === 0) {
+            number = 'Fizz';
+        }else{
+            number = i;
+        }
+        console.log(number);
+        let secondNumberBox = document.querySelector('#second-number-box');
+        let secondBox = document.createElement('div');
+        secondBox.innerHTML = number;
+        secondBox.classList.add('box');
+        secondBox.classList.add(number);
+        secondNumberBox.append(secondBox);
+    }
