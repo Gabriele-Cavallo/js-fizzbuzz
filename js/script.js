ffc5061e1f4fc645f129b6d6f1efcb65fb8ecee5
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-08 10:57:54 +0100

    fix: general syntax

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@
 // // Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
 // BONUS 2:
 // Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
-
+// Prendere il contenitore a cui appendere i box.
+let numberBox = document.querySelector('#number-box');
 // 1 - Stampo in console i numeri da 1 a 100
 //Primo container
 for (let i = 1; i <= 100; i++){
@@ -16,38 +17,48 @@ for (let i = 1; i <= 100; i++){
     // 2-b Controllo se il numero è divisibile per 5 e gli assegno il valore buzz
     // 2-c Controllo se il numero è divisibile per 3 e gli assegno il valore fizz
     let number;
+    let cssClass;
     if (i % 3 === 0 && i % 5=== 0) {
         number = 'FizzBuzz';
+        cssClass = 'fizzbuzz';
     }else if (i % 5 === 0) {
         number = 'Buzz';
+        cssClass = 'buzz';
     }else if (i % 3 === 0) {
         number = 'Fizz';
+        cssClass = 'fizz';
     }else{
         number = i;
+        cssClass = 'base-bg';
     }
 // 3 - Stampo in console i nuovi valori
     console.log(number);
-// 4 - Aggiungo un elemento al DOM per stampare in pagina i valori
-    let numberBox = document.querySelector('#number-box');
-    let box = `<div class="box ${number}">${number}</div>`;
+// 4 - Aggiungo un box con il messaggio.
+    let box = `<div class="box ${cssClass}">${number}</div>`;
     numberBox.innerHTML += box;
 }
 //Secondo container
+let secondNumberBox = document.querySelector('#second-number-box');
 for (let i = 1; i <= 100; i++){
     let number;
+    let cssClass;
     if (i % 3 === 0 && i % 5=== 0) {
         number = 'FizzBuzz';
+        cssClass = 'fizzbuzz';
     }else if (i % 5 === 0) {
         number = 'Buzz';
+        cssClass = 'buzz';
     }else if (i % 3 === 0) {
         number = 'Fizz';
+        cssClass = 'fizz';
     }else{
         number = i;
+        cssClass = 'base-bg';
     }
-    let secondNumberBox = document.querySelector('#second-number-box');
     let secondBox = document.createElement('div');
     secondBox.innerHTML = number;
     secondBox.classList.add('box');
     secondBox.classList.add(number);
+    secondBox.classList.add(cssClass);
     secondNumberBox.append(secondBox);
 }
